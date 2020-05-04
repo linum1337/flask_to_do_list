771699e68308f9ff89cf5114848b0e86cd1c2b55
slonj <arsenya-kopylov@yandex.ru>
2020-05-04 12:32:26 +0300

    Всё работает. Хеширование пароля сделал. Повторно пароли одинаковые не создаются в бд.

---
+++ page.py
@@ -0,0 +1,78 @@
+from flask import Flask, redirect, render_template
+from AuthForm import LoginForm
+from RegForm import RegForm
+from data.users import User
+from data import db_session
+import os, hashlib
+
+
+app = Flask(__name__)
+salt = os.urandom(32)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/registration', methods=['GET'])
+def registration():
+    form = RegForm()
+    return render_template('registration.html', title='Регистрация', form=form)
+
+
+@app.route('/')
+@app.route('/login', methods=['GET'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/check_reg', methods=['POST'])
+def check_reg():
+    form = RegForm()
+    session = db_session.create_session()
+    password = form.password.data
+    password_again = form.password_again.data
+    if password == password_again :
+        if len(password) > 8 :
+            if not password.isdigit() :
+                digit_in = False
+                for i in range(10) :
+                    if str(i) in password :
+                        digit_in = True
+                if digit_in :
+                    key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000, dklen=128)
+                    if not session.query(User).filter(User.name == form.username.data,
+                                                      User.hashed_password == key).first():
+                        u = User()
+                        u.name = form.username.data
+                        u.hashed_password = key
+                        session.add(u)
+                        session.commit()
+                    return redirect('/login')
+                else :
+                    return render_template('error_reg.html' , title='Регистрация' , form=form)
+            else :
+                return render_template('error_reg.html' , title='Регистрация' , form=form)
+        else :
+            return render_template('error_reg.html' , title='Регистрация' , form=form)
+    else :
+        return render_template('error_reg.html' , title='Регистрация' , form=form)
+
+
+@app.route('/check_log', methods=['POST'])
+def check_log():
+    form = LoginForm()
+    session = db_session.create_session()
+    key = hashlib.pbkdf2_hmac('sha256' , form.password.data.encode('utf-8') , salt , 100000 , dklen=128)
+    if session.query(User).filter(User.name == form.username.data, User.hashed_password == key).first() :
+        return redirect('/list')
+    else:
+        return render_template('error_log.html', title='Авторизация' , form=form)
+
+
+@app.route('/list')
+def list():
+    return render_template('main_page.html', title='To_do')
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/users.sqlite")
+    app.run(port=8080, host='127.0.0.1')
