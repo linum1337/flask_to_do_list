0ea5588da0456dba2661720e62a8c0817d4c361a
linum <lebishova.elena@mail.ru>
2020-05-05 20:45:09 +0300

    Убрал лишнее для работы с хироку

--- page.py
+++ page.py
@@ -5,10 +5,33 @@ from data.users import User
 from data import db_session
 import os, hashlib
 
-
 app = Flask(__name__)
 salt = os.urandom(32)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = StringField('Пароль', validators=[DataRequired()])
+    submitL = SubmitField('Авторизоваться')
+    butreg = SubmitField('Зарегистрироваться')
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class RegForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = StringField('Пароль', validators=[DataRequired()])
+    password_again = StringField('Подтвердите пароль', validators=[DataRequired()])
+    submitR = SubmitField('Зарегистрироваться')
+
 
 @app.route('/month_change')
 def month_change():
@@ -34,14 +57,14 @@ def check_reg():
     session = db_session.create_session()
     password = form.password.data
     password_again = form.password_again.data
-    if password == password_again :
-        if len(password) > 8 :
-            if not password.isdigit() :
+    if password == password_again:
+        if len(password) > 8:
+            if not password.isdigit():
                 digit_in = False
-                for i in range(10) :
-                    if str(i) in password :
+                for i in range(10):
+                    if str(i) in password:
                         digit_in = True
-                if digit_in :
+                if digit_in:
                     key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000, dklen=128)
                     if not session.query(User).filter(User.name == form.username.data,
                                                       User.hashed_password == key).first():
@@ -51,25 +74,25 @@ def check_reg():
                         session.add(u)
                         session.commit()
                     return redirect('/login')
-                else :
-                    return render_template('error_reg.html' , title='Регистрация' , form=form)
-            else :
-                return render_template('error_reg.html' , title='Регистрация' , form=form)
-        else :
-            return render_template('error_reg.html' , title='Регистрация' , form=form)
-    else :
-        return render_template('error_reg.html' , title='Регистрация' , form=form)
+                else:
+                    return render_template('error_reg.html', title='Регистрация', form=form)
+            else:
+                return render_template('error_reg.html', title='Регистрация', form=form)
+        else:
+            return render_template('error_reg.html', title='Регистрация', form=form)
+    else:
+        return render_template('error_reg.html', title='Регистрация', form=form)
 
 
 @app.route('/check_log', methods=['POST'])
 def check_log():
     form = LoginForm()
     session = db_session.create_session()
-    key = hashlib.pbkdf2_hmac('sha256' , form.password.data.encode('utf-8') , salt , 100000 , dklen=128)
-    if session.query(User).filter(User.name == form.username.data, User.hashed_password == key).first() :
+    key = hashlib.pbkdf2_hmac('sha256', form.password.data.encode('utf-8'), salt, 100000, dklen=128)
+    if session.query(User).filter(User.name == form.username.data, User.hashed_password == key).first():
         return redirect('/month_change')
     else:
-        return render_template('error_log.html', title='Авторизация' , form=form)
+        return render_template('error_log.html', title='Авторизация', form=form)
 
 
 @app.route('/list')
