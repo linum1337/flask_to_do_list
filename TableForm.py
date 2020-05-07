0b43f1665b0fa3a6d80e4392f01514cddae1bc37
linum <lebishova.elena@mail.ru>
2020-05-07 21:38:47 +0300

    Все работает кроме названия дня

--- TableForm.py
+++ TableForm.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList
+from wtforms import StringField, SubmitField, FieldList, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class TableForm(FlaskForm):
-    list_days = FieldList(StringField('название дня'), min_entries=31)
+    list_days = FieldList(TextAreaField('название дня'), min_entries=31)
     submit_save = SubmitField('Сохранить')
