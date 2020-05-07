b21d79a97ced6c8751c60394099aad7a4d88fab9
slonj <arsenya-kopylov@yandex.ru>
2020-05-07 14:10:22 +0300

    !

--- TableForm.py
+++ TableForm.py
@@ -4,10 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class TableForm(FlaskForm):
-    list_days_text = []
-    list_days_names = []
-    for i in range(31):
-        list_days_names.append(StringField('название дня', validators=[DataRequired()]))
-        list_days_text.append(StringField('', validators=[DataRequired()]))
+    list_days_names = ['' for i in range(31)]
+    list_days_text = ['' for j in range(31)]
     name_month = StringField('название месяца', validators=[DataRequired()])
     submit_save = SubmitField('Сохранить')
