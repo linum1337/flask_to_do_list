a110d79e9c5ee5234cf49112b98587ebb24f4069
slonj <arsenya-kopylov@yandex.ru>
2020-05-08 13:35:21 +0300

    !

---
+++ data/Days.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class DaysInfo(SqlAlchemyBase):
+    __tablename__ = 'DaysInfo'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    important = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
+    key_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    data = sqlalchemy.Column(sqlalchemy.DATE, nullable=True)
+
