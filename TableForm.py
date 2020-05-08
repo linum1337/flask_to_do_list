31f363ad2cae7c01fa559b1f08b97d4e1d33fbb5
slonj <arsenya-kopylov@yandex.ru>
2020-05-08 14:43:07 +0300

    !

--- TableForm.py
+++ TableForm.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList, TextAreaField
+from wtforms import SubmitField, FieldList, TextAreaField, DateTimeField
 from wtforms.validators import DataRequired
 
 
 class TableForm(FlaskForm):
     list_days = FieldList(TextAreaField('название дня'), min_entries=35)
+    list_dates = FieldList(DateTimeField(''), min_entries=35)
     submit_save = SubmitField('Сохранить')
--- page.py
+++ page.py
@@ -5,13 +5,13 @@ from TableForm import TableForm
 from data import db_session
 from data.users import User
 from data.Days import DaysInfo
-from flask_ngrok import run_with_ngrok
 import datetime
+import calendar
 
 
 app = Flask(__name__)
-run_with_ngrok(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+user_name = ''
 
 
 @app.route('/registration', methods=['GET'])
@@ -31,6 +31,7 @@ def login():
 def check_reg():
     session = db_session.create_session()
     form = RegForm()
+    today = datetime.datetime.today()
     password = form.password.data
     password_again = form.password_again.data
     if password == password_again :
@@ -49,9 +50,10 @@ def check_reg():
                                 u.hashed_password = password
                                 session.add(u)
                                 session.commit()
-                                for i in range(31):
+                                for i in range(35):
                                     d = DaysInfo()
-                                    d.key_id = u.id
+                                    d.data = today + datetime.timedelta(days=i)
+                                    d.user_name = u.name
                                     session.add(d)
                                 session.commit()
 
@@ -71,6 +73,8 @@ def check_reg():
 @app.route('/check_log', methods=['POST'])
 def check_log():
     form = LoginForm()
+    global user_name
+    user_name = form.username.data
     session = db_session.create_session()
     if session.query(User).filter(User.name == form.username.data, User.hashed_password == form.password.data).first():
         return redirect('/list')
@@ -80,27 +84,32 @@ def check_log():
 
 @app.route('/save_info', methods=['POST'])
 def save_info():
+    global user_name
     session = db_session.create_session()
     form = TableForm()
     if form.validate_on_submit():
-        days_info_texts = session.query(DaysInfo).filter(DaysInfo.key_id == User.id).all()
-        for i in range(len(form.list_days)):
-            days_info_texts[i].text = form.list_days[i].data
-        session.commit()
+        if session.query(DaysInfo).filter(user_name == DaysInfo.user_name).all():
+            days_info_texts = session.query(DaysInfo).filter(user_name == DaysInfo.user_name).all()
+            for i in range(len(form.list_days)):
+                print(days_info_texts[i].text)
+                days_info_texts[i].text = form.list_days[i].data
+            session.commit()
     return redirect('/list')
 
 
 @app.route('/list', methods=['GET'])
 def list():
+    global user_name
     form = TableForm()
     session = db_session.create_session()
-    days_info_texts = session.query(DaysInfo).filter(DaysInfo.key_id == 1).all()
-    for i in range(30):
+    days_info_texts = session.query(DaysInfo).filter(DaysInfo.user_name == user_name).all()
+    for i in range(35):
         form.list_days[i].data = days_info_texts[i].text
+        form.list_dates[i].data = days_info_texts[i].data
     return render_template('main_page.html', title='To_do', form=form)
 
 
 if __name__ == '__main__':
     db_session.global_init("db/users.sqlite")
     db_session.global_init("db/days.sqlite")
-    app.run()
+    app.run(port=8080 , host='127.0.0.1', debug=True)
